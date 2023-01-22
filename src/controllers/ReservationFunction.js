fe3d3cbc55cc743340ae968bbe807c53a84476c7
Henry Chun <chunhyungjoon@gmail.com>
2023-01-22 20:40:31 +1100

    find decfections

--- src/controllers/ReservationFunction.js
+++ src/controllers/ReservationFunction.js
@@ -1,69 +1,89 @@
-import Reservation from "../db/models/ReservationModel.js";
-import Table from '../db/models/TableModel.js'
+import Reservation from '../db/models/ReservationModel.js';
+import Table from '../db/models/TableModel.js';
 
 function manipulateHours(date, operator, hours) {
   switch (operator) {
     case 'plus':
-      return new Date(date.getTime() + 60*60000*hours)
+      return new Date(date.getTime() + 60 * 60000 * hours);
     case 'minus':
-      return new Date(date.getTime() - 60*60000*hours)   
+      return new Date(date.getTime() - 60 * 60000 * hours);
     default:
-      return new Error(`Given operator ${operator} is invalid operator`)
+      return new Error(`Given operator ${operator} is invalid operator`);
   }
 }
 
 function findDuplicateReservation(bookingInfo, reservations) {
-  return reservations.find(reservation => reservation.guest.mobile === bookingInfo.mobile)
+  return reservations.find(
+    (reservation) => reservation.guest.mobile === bookingInfo.mobile
+  );
 }
 
 export async function getUnavailableTables(req, res, next) {
-  const bookingInfo = req.body
-
+  const bookingInfo = req.body;
   const dateFilteredReservations = await Reservation.find({
-    'guest.date': {$lt: manipulateHours(new Date(bookingInfo.date), 'plus', 5), 
-                   $gt: manipulateHours(new Date(bookingInfo.date), 'minus', 5)}
-                  }).populate('table')
+    'guest.date': {
+      $lt: manipulateHours(new Date(bookingInfo.date), 'plus', 5),
+      $gt: manipulateHours(new Date(bookingInfo.date), 'minus', 5),
+    },
+  }).populate('table');
 
+  console.log('*****', req.body);
   if (dateFilteredReservations.length) {
-    const duplicateReservation = findDuplicateReservation(bookingInfo, dateFilteredReservations)
+    const duplicateReservation = findDuplicateReservation(
+      bookingInfo,
+      dateFilteredReservations
+    );
     if (duplicateReservation) {
-      res.status(409).json({error: `Same guest found!`})
-    }
-    else {
-      const unavailableTables = dateFilteredReservations.filter(reservation => 
-        reservation.guest.date < manipulateHours(new Date(bookingInfo.date), 'plus', 1.5) &&
-        reservation.guest.date > manipulateHours(new Date(bookingInfo.date), 'minus', 1.5) 
-        ).filter(reservation => 
-          reservation.table.seats === bookingInfo.guestNumber || 
-          reservation.table.seats === bookingInfo.guestNumber+ 1
-          ).map(reservation => reservation.table)
+      res.status(409).json({ error: `Same guest found!` });
+    } else {
+      const unavailableTables = dateFilteredReservations
+        .filter(
+          (reservation) =>
+            reservation.guest.date <
+              manipulateHours(new Date(bookingInfo.date), 'plus', 1.5) &&
+            reservation.guest.date >
+              manipulateHours(new Date(bookingInfo.date), 'minus', 1.5)
+        )
+        .filter(
+          (reservation) =>
+            reservation.table.seats === bookingInfo.guestNumber ||
+            reservation.table.seats === bookingInfo.guestNumber + 1
+        )
+        .map((reservation) => reservation.table);
 
-      req.unavailableTables = unavailableTables
-      next()
-    } 
-  }else{
-    next()
+      req.unavailableTables = unavailableTables;
+      next();
+    }
+  } else {
+    next();
   }
 }
 
 export async function getAvailableTable(req, res, next) {
   if (!req.unavailableTables || !req.unavailableTables.length) {
-    const allTables = await Table.find()
+    const allTables = await Table.find();
     req.availableTableId = allTables.find(
-      table => table.seats === req.body.guestNumber || table.seats === req.body.guestNumber + 1
-      )._id
-    next()
-  }else {
-    const seatFilteredTables = await Table.find({seats: req.unavailableTables[0].seats})
-    const availableTable = seatFilteredTables.find(table => 
-      !req.unavailableTables.map(unavailableTable => unavailableTable.tableNumber).includes(table.tableNumber))
-    
+      (table) =>
+        table.seats === req.body.guestNumber ||
+        table.seats === req.body.guestNumber + 1
+    )._id;
+    next();
+  } else {
+    const seatFilteredTables = await Table.find({
+      seats: req.unavailableTables[0].seats,
+    });
+    const availableTable = seatFilteredTables.find(
+      (table) =>
+        !req.unavailableTables
+          .map((unavailableTable) => unavailableTable.tableNumber)
+          .includes(table.tableNumber)
+    );
+
     if (!availableTable) {
-      res.status(406).json({error: 'No available table found'})
-    }else {
-    req.availableTableId = availableTable._id
-    next()
+      res.status(406).json({ error: 'No available table found' });
+    } else {
+      req.availableTableId = availableTable._id;
+      next();
     }
   }
 }
-
--- src/controllers/ReservationRoutes.js
+++ src/controllers/ReservationRoutes.js
@@ -1,42 +1,66 @@
-import express from 'express'
-import { verifyJwt, verifyCredentials, generateAdminJWT } from './AdminFunction.js' //recently uncommented.
-import { 
-  getUnavailableTables, 
+import express from 'express';
+import {
+  verifyJwt,
+  verifyCredentials,
+  generateAdminJWT,
+} from './AdminFunction.js'; //recently uncommented.
+import {
+  getUnavailableTables,
   getAvailableTable,
+} from './ReservationFunction.js';
+import Reservation from '../db/models/ReservationModel.js';
 
-} from './ReservationFunction.js'
-import Reservation from '../db/models/ReservationModel.js'
-
-const router = express.Router()
+const router = express.Router();
 
 //GET ALL RESERVATIONS
-router.get('/', verifyJwt, verifyCredentials, generateAdminJWT, async (req, res) => {
-  const reservations = await Reservation.find().populate({path: 'table', select: ['tableNumber', 'seats']})
-  res.send(reservations)
-})
+router.get(
+  '/',
+  verifyJwt,
+  verifyCredentials,
+  generateAdminJWT,
+  async (req, res) => {
+    const reservations = await Reservation.find().populate({
+      path: 'table',
+      select: ['tableNumber', 'seats'],
+    });
+    res.send(reservations);
+  }
+);
 
 //POST
 router.post('/', getUnavailableTables, getAvailableTable, async (req, res) => {
+  console.log('####', req.body);
+  req.body.date = new Date(req.body.date);
   const newBooking = await Reservation.create({
     table: req.availableTableId,
-    guest: req.body
-  })
-  res.status(201).send(await newBooking.populate({path: 'table', select: ['tableNumber', 'seats']}))
-})
+    guest: req.body,
+  });
+  console.log('####', req.body);
+  res.status(201).send(
+    await newBooking.populate({
+      path: 'table',
+      select: ['tableNumber', 'seats'],
+    })
+  );
+});
 
 //GET BY MOBILE
-router.get('/:mobile', verifyJwt, verifyCredentials, generateAdminJWT, async (req, res) => {
-  const reservations = await Reservation.find({'guest.mobile': req.params.mobile})
-  .populate({path: 'table', select: ['tableNumber', 'seats']})
+router.get(
+  '/:mobile',
+  verifyJwt,
+  verifyCredentials,
+  generateAdminJWT,
+  async (req, res) => {
+    const reservations = await Reservation.find({
+      'guest.mobile': req.params.mobile,
+    }).populate({ path: 'table', select: ['tableNumber', 'seats'] });
 
-  res.send(reservations)
-})
+    res.send(reservations);
+  }
+);
 
 //UPDATE ONE BY ID
 
-
 //DELETE ONE BY ID
 
-
-export default router
-
+export default router;
